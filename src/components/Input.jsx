f8336f396afa48624adeb997a2d5c5145bc95edd
Rajeev <ronaldocrcr5@gmail.com>
2024-03-07 00:01:08 +0530

    configure messages and message component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Img from "../images/img.png";
+import Attach from "../images/attach.png";
 
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="Type here..." />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="text" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/20180718/pexels-photo-20180718/free-photo-of-taking-a-break.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img
+          src="https://images.pexels.com/photos/20180718/pexels-photo-20180718/free-photo-of-taking-a-break.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
