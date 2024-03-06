04c95439bd8d6758156f6df10e3e2e0aea8c1324
Rajeev <ronaldocrcr5@gmail.com>
2024-03-06 22:36:26 +0530

    Styled chat and chats components successfully

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Cam from "../images/cam.png";
+import Add from "../images/add.png";
+import More from "../images/more.png";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -13,6 +13,28 @@ const Chats = () => {
           <p>Hello</p>
         </div>
       </div>
+
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/20180718/pexels-photo-20180718/free-photo-of-taking-a-break.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/20180718/pexels-photo-20180718/free-photo-of-taking-a-break.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   );
 };
