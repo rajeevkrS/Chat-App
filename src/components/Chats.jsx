dcc7504e9c88907d1ac7163e149aa86dbd400428
Rajeev <ronaldocrcr5@gmail.com>
2024-03-10 14:20:08 +0530

    fixed some issues in chats compnent.

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -24,13 +24,15 @@ const Chats = () => {
       };
     };
 
+    // If there is current user id after that call the getChats() func.
     currentUser.uid && getChats();
   }, [currentUser.uid]);
 
-  console.log(Object.entries(chats));
+  // console.log(Object.entries(chats));
 
   return (
     <div className="chats">
+      {/* Chats here is in object fromat "{}" which is getting from onSnapshot method, but for mapping over "chats" we need to conert it into array so to convert I used a method Object.entries() method which converts it into array "[]". */}
       {Object.entries(chats)?.map((chat) => (
         <div className="userChat" key={chat[0]}>
           <img src={chat[1].userInfo.photoURL} alt="" />
